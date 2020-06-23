833b40ce1c03a0151b67b99f084ba7b0766a3f0c
leaf_eecs <leafeecs@gmail.com>
2020-06-23 13:07:15 +0900

    [Add] Make New Folders and Done Lab 1 Checkoff

---
+++ lab/lab1/HelloNumbers.java
@@ -0,0 +1,10 @@
+public class HelloNumbers {
+	public static void main(String[] args) {
+		int sum = 0;
+
+		for (int num = 0; num < 10; num++) {
+			sum += num;
+		}
+		System.out.println(sum);
+	}
+}
